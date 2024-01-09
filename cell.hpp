4ae78244399be22f25f195608cc4c027fb0f5d2c
Roland DENIS <denis@math.univ-lyon1.fr>
2024-01-09 14:18:10 +0100

    Overload bitwise shift operators for interval and using it instead of */

--- cell.hpp
+++ cell.hpp
@@ -4,31 +4,23 @@
 #include <ostream>
 #include <type_traits>
 
-#include "interval.hpp"
-
 namespace details
 {
-    /// Bitwise shift with signed shift
-    template <
-        typename T,
-        typename std::enable_if_t<std::is_integral_v<T>, bool> = true
-    >
-    constexpr T bitwise_shift(T i, std::ptrdiff_t shift) noexcept
+    /// Bitwise shift with signed shift (<< for positive shift, >> for negative shift)
+    template <typename T>
+    constexpr auto bitwise_shift(T && i, std::ptrdiff_t shift) noexcept
     {
         if (shift >= 0)
-            return i * (1 << shift);
+            return std::forward<T>(i) << shift;
         else
-            return i / (1 << -shift);       
+            return std::forward<T>(i) >> -shift;     
     }
 
     /// Shift level and position of one index
-    template <
-        typename T,
-        typename std::enable_if_t<std::is_integral_v<T>, bool> = true
-    >
-    constexpr T apply(T i, std::ptrdiff_t index_shift, std::ptrdiff_t level_shift) noexcept
+    template <typename T>
+    constexpr auto apply(T && i, std::ptrdiff_t index_shift, std::ptrdiff_t level_shift) noexcept
     {
-        return bitwise_shift(i, level_shift) + index_shift;
+        return bitwise_shift(std::forward<T>(i), level_shift) + index_shift;
     }
 
 } // namespace details
@@ -49,9 +41,13 @@ struct KCell
 {
     /// Topology of the cell ({0, 1} in 1D, 0...3 in 2D, 0...7 in 3D, ...) => can be used to index a dedicated storage
     using topology_type = unsigned int;
+    using dimension_type = unsigned int;
 
     /// Topology of the cell as an unsigned integer (0 <=> open, 1 <=> close)
     static constexpr topology_type topology() noexcept { return static_cast<topology_type>(Open); }
+
+    /// Dimension of the cell
+    static constexpr dimension_type dimension() noexcept { return static_cast<dimension_type>(Open); }
 
     /// Khalimsky coordinate of the cell
     static constexpr std::ptrdiff_t khalimsky() noexcept { return 2 * IndexShift + Open; }
@@ -79,26 +75,15 @@ struct KCell
     template <std::ptrdiff_t Levels = 1>
     static constexpr KCell<Open, details::apply(IndexShift, 0, -Levels), LevelShift - Levels> down() noexcept { return {}; }
 
-    /// Apply the level/index shifts of the current cell to a given index
+    /// Apply the level/index shifts of the current cell to a given index (or interval)
     template <typename T>
-    static constexpr auto apply(T i) noexcept -> decltype(details::apply(i, IndexShift, LevelShift))
+    static constexpr auto apply(T && i) noexcept
     {
-        return details::apply(i, IndexShift, LevelShift);
+        return details::apply(std::forward<T>(i), IndexShift, LevelShift);
     }
 
-    /// Apply the level/index shifts of the current cell to a given interval
-    static constexpr Interval apply(Interval const& i)
-    {
-        auto step = details::bitwise_shift(i.step, LevelShift);
-        return {
-            apply(i.a),
-            apply(i.b),
-            step > 0 ? step : 1
-        };
-    }
-
-    template <typename Function>
-    static constexpr void apply(Function && fn, Interval const& i) { std::forward<Function>(fn)(apply(i)); }
+    template <typename Function, typename Index>
+    static constexpr void apply(Function && fn, std::size_t level, Index && i) { std::forward<Function>(fn)(level + LevelShift, apply(std::forward<Index>(i))); }
 };
 
 template <
